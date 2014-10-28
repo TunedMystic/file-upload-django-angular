a015400ce0c5d78ee844463ba1cebc22cdbdb2d7
Sandeep Jadoonanan <TunedMystic@users.noreply.github.com>
2014-10-28 01:19:33 -0400

    Update settings.py

--- portal/settings.py
+++ portal/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar.apps.DebugToolbarConfig',
-	"django_extensions",
+    "django_extensions",
     "multiupload",
 )
 
