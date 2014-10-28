b4c121d8fd07c3fcd39968c0aa63c0d6f28505b7
Sandeep Jadoonanan <TunedMystic@users.noreply.github.com>
2014-10-28 01:18:57 -0400

    Add 'debug-toolbar' and 'django_extensions' apps.

--- portal/settings.py
+++ portal/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "multiupload"
+    'debug_toolbar.apps.DebugToolbarConfig',
+	"django_extensions",
+    "multiupload",
 )
 
 MIDDLEWARE_CLASSES = (
